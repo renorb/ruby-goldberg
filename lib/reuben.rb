37e1884196eecb9407fbcf71d21b7160a72d8c91
spovich <spovich@gmail.com>
2010-03-11 21:21:30 -0700

    update server

--- lib/reuben.rb
+++ lib/reuben.rb
@@ -5,7 +5,7 @@ module Rack
   class Reuben
 
     def initialize
-      # cache = Memcached.new("localhost:11211")
+      @cache = Memcached.new("localhost:11211") # setup memcached connection
     end
     
     def call(env)
@@ -15,7 +15,20 @@ module Rack
       when "/" # not sure what root should return
         "Nothing Here"
       when "/rubeme" # register a rube client
-        "rubeme"
+        if request.post?
+          name = request.params["name"]
+          rube = request.params["rube"]
+          health = request.params["health"]
+          begin
+            @cache.set "#{name}_name", name
+            @cache.set "#{name}_rube", rube
+            @cache.set "#{name}_health", health
+          rescue Memcached::NotFound
+          end
+          "registered name:#{name} rube url:#{rube} health url:#{health}"
+        else
+          "fail"
+        end
       else
         "Unknown"
       end
