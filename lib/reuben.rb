7292bb426d8cb4b9172d8a1d4eb993ad938bbad2
bitbckt <brandon@systemisdown.net>
2010-03-11 21:16:24 -0800

    request.params should be params, under Sinatra.

--- lib/reuben.rb
+++ lib/reuben.rb
@@ -13,19 +13,24 @@ module Rack
         @cache.set "keys", [] # reset key array to empty array
       end
     end
-    
-    get "/" do # render form to input text to be rube'ified
+
+    get "/rubes" do # get list of registered rube's
+      keys = @cache.get "keys"
+      "rubes: #{keys}"
+    end
+
+    get "/" do # not sure what root should return
       erb :index
     end
 
-    post "/" do # kick off 
+    post "/" do # kick off
       # process posted text - pass it to a random rube
     end
-    
+
     post "/rubeme" do # register a rube client
-      name = request.params["name"]
-      rube = request.params["rube"]
-      health = request.params["health"]
+      name   = params["name"]
+      rube   = params["rube"]
+      health = params["health"]
 
       @cache.set "#{name}_name", name
       @cache.set "#{name}_rube", rube
@@ -41,7 +46,7 @@ module Rack
       keys = @cache.get "keys"
       "rubes: #{keys}"
     end
-      
+
     get "/check" do # "/check?test" will check if client named 'test' is registered
       begin
         result = @cache.get "#{request.GET}_name"
@@ -50,6 +55,6 @@ module Rack
         "#{request.GET} not found"
       end
     end
-    
+
   end
 end
