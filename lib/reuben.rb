9da068333ef1864e53bf96b8c11ad267a90ff4c1
spovich <spovich@gmail.com>
2010-03-11 22:16:10 -0700

    check

--- lib/reuben.rb
+++ lib/reuben.rb
@@ -7,23 +7,39 @@ module Rack
 
     def initialize
       @cache = Memcached.new("localhost:11211") # setup memcached connection
+      begin
+        @cache.get "keys" # see if keys array is set
+      rescue Memcached::NotFound
+        @cache.set "keys", [] # reset key array to empty array
+      end
     end
     
-    get "/" do # not sure what root should return
+    get "/" do # render form to input text to be rube'ified
       erb :index
+    end
+
+    post "/" do # kick off 
+      # process posted text - pass it to a random rube
     end
     
     post "/rubeme" do # register a rube client
       name = request.params["name"]
       rube = request.params["rube"]
       health = request.params["health"]
-      begin
-        @cache.set "#{name}_name", name
-        @cache.set "#{name}_rube", rube
-        @cache.set "#{name}_health", health
-      rescue Memcached::NotFound
-      end
+
+      @cache.set "#{name}_name", name
+      @cache.set "#{name}_rube", rube
+      @cache.set "#{name}_health", health
+
+      keys = @cache.get "keys"
+      keys << name
+      @cache.set "keys", keys
       "registered name:#{name} rube url:#{rube} health url:#{health}"
+    end
+
+    get "/rubes" do # get list of registered rube's
+      keys = @cache.get "keys"
+      "rubes: #{keys}"
     end
       
     get "/check" do # "/check?test" will check if client named 'test' is registered
